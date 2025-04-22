cfd81b5851433e9619a71ba0868421e33fcd7308
greyart93 <119904509+greyart93@users.noreply.github.com>
2025-04-22 08:27:20 +0530

    Muliti-linear_reg.py

--- linear_reg.py
+++ linear_reg.py
@@ -30,3 +30,28 @@ r2 = r2_score(y_test, y_pred)
 print("Simple Linear Regression:")
 print("Mean Squared Error:", mse)
 print("R-squared:", r2)
+print("Coefficients:", simple_regressor.coef_)
+print("Intercept:", simple_regressor.intercept_)
+
+X_multiple = data[['cgpa', 'package']] 
+X_multiple_train, X_multiple_test, y_multiple_train, y_multiple_test = train_test_split(X_multiple, y, test_size=0.2, random_state=42)
+
+
+multiple_regressor = LinearRegression()
+multiple_regressor.fit(X_multiple_train, y_multiple_train)
+
+
+y_multiple_pred = multiple_regressor.predict(X_multiple_test)
+
+
+mse_multiple = mean_squared_error(y_multiple_test, y_multiple_pred)
+r2_multiple = r2_score(y_multiple_test, y_multiple_pred)
+
+
+
+
+print("\nMultiple Linear Regression:")
+print("Coefficients:", multiple_regressor.coef_)
+print("Intercept:", multiple_regressor.intercept_)
+print("Mean Squared Error:", mse_multiple)
+print("R-squared:", r2_multiple)
